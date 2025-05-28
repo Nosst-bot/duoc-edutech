06c8c9b3cb29723f00fd862e0d00d4daffda2ddf
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2025-05-28 17:02:35 -0400

    Modulo Usuario y autenticacion completado

--- src/main/java/com/edutech/edutech/model/Preferencia.java
+++ src/main/java/com/edutech/edutech/model/Preferencia.java
@@ -1,12 +1,11 @@
 package com.edutech.edutech.model;
 
-import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+
 
 @Entity
 public class Preferencia {
