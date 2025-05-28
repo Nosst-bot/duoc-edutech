d2e282ef75eb84d4edae68617837f2d4b46ec402
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2025-05-28 14:45:37 -0400

    entidades Persona y Usuario Testeadas

--- src/main/java/com/edutech/edutech/model/Persona.java
+++ src/main/java/com/edutech/edutech/model/Persona.java
@@ -1,9 +1,12 @@
 package com.edutech.edutech.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Persona {
@@ -11,12 +14,15 @@ public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idPersona;
-
+    
+    @Column(unique = true)
     private String rut;
     private String nombre;
     private String apellido;
     private String mail;
 
+    @OneToOne(mappedBy = "persona", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Usuario usuario;
 
     public Persona(int idPersona, String rut, String nombre, String apellido, String mail) {
         this.idPersona = idPersona;
@@ -25,43 +31,37 @@ public class Persona {
         this.apellido = apellido;
         this.mail = mail;
     }
+    public Persona() {
+}
+
 
     public int getIdPersona() {
         return idPersona;
     }
-
     public void setIdPersona(int idPersona) {
         this.idPersona = idPersona;
     }
-
     public String getRut() {
         return rut;
     }
-
     public void setRut(String rut) {
         this.rut = rut;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getApellido() {
         return apellido;
     }
-
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
     public String getMail() {
         return mail;
     }
-
     public void setMail(String mail) {
         this.mail = mail;
     }
--- src/main/java/com/edutech/edutech/model/Usuario.java
+++ src/main/java/com/edutech/edutech/model/Usuario.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 
 @Entity
@@ -18,55 +18,45 @@ public class Usuario {
     private String mail;
     private String pass;
     private String estado;
-
-    @ManyToOne
-    @JoinColumn(name = "idPersona")
+    @OneToOne
+    @JoinColumn(name = "idPersona", unique = true)
     private Persona persona;
 
-    public Usuario(int idUsuario, String mail, String pass, String estado, Persona persona) {
+    public Usuario() {}
+
+    public Usuario(int idUsuario, String mail, String pass, String estado) {
         this.idUsuario = idUsuario;
         this.mail = mail;
         this.pass = pass;
         this.estado = estado;
-        this.persona = persona;
     }
-
     public int getIdUsuario() {
         return idUsuario;
     }
-
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
-
     public String getMail() {
         return mail;
     }
-
     public void setMail(String mail) {
         this.mail = mail;
     }
-
     public String getPass() {
         return pass;
     }
-
     public void setPass(String pass) {
         this.pass = pass;
     }
-
     public String getEstado() {
         return estado;
     }
-
     public void setEstado(String estado) {
         this.estado = estado;
     }
-
     public Persona getPersona() {
         return persona;
     }
-
     public void setPersona(Persona persona) {
         this.persona = persona;
     }
